44113e1da609f5d607973cdff68625481ff7c2bf
xkz <xkz@sky233.com>
2022-07-31 11:14:54 +0800

    Refactoring API

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask
+from flask import Flask,redirect,abort
 import os
 import random
 
-text = open("text.txt", encoding='utf-8')
-msg = text.readlines()
-
 app = Flask(__name__)
 
-
 @app.route('/')
-def getmsg():
-    return random.choice(msg)
+def index():
+    return redirect('/api/v2/zdd')
+# Maintain compatibility with original version of 188api
 
+@app.route('/api/v2/<string:name>')
+def getmsg(name):
+    try:
+        text = open("text/"+name+".txt", encoding='utf-8')
+    except FileNotFoundError:
+        abort(404)
+# Check if file exist
+    else:
+        msg = text.readlines()
+        return random.choice(msg)
+# Random select one line and return
 
 if __name__ == '__main__':
     app.run()
